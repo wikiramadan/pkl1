f10b6e7fad88c72937a9983bbf64c224a299e65e
afifiabdulhadi <afifiabdulhadi0.gmail.com>
2024-11-20 14:00:35 +0700

    edit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -31,7 +31,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="navbar" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="contak" class="nav-link">Contact</a>
       </li>
     </ul>
 
